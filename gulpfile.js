3a65f1315e163e3a96881e494375542ba36bb3cf
Gizelly Stefanny <gizellysteffanny@gmail.com>
2018-02-20 22:28:48 -0400

    Install gulp-imagemin & created task to compress images

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,15 @@ var
     gulp = require('gulp'),
     sass = require('gulp-sass'),
     concat = require('gulp-concat'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin');
+    //pngquant = require('imagemin-quant');
 
 
-// my tasks
-gulp.task('default', ['sass', 'js']);
+// run all tasks
+gulp.task('default', ['sass', 'js', 'image']);
 
-// compiled and compressed css files
+// compile, concat and  compress css files
 gulp.task('sass', function () {
     return gulp.src('assets/src/sass/**/*.scss')
         .pipe(concat ('style.min.css'))
@@ -16,9 +18,27 @@ gulp.task('sass', function () {
         .pipe( gulp.dest('assets/css'));
 });
 
+// concat and compress
 gulp.task('js', function () {
     return gulp.src('assets/src/js/**/*.js')
     .pipe(concat ('app.min.js'))
-        .pipe(uglify())
-        .pipe( gulp.dest('assets/js'));
+    .pipe(uglify())
+    .pipe( gulp.dest('assets/js'));
+});
+
+// minify images (all format)
+gulp.task('image', function () {
+    return gulp.src('assets/src/images/*')
+    .pipe(imagemin([
+        imagemin.gifsicle({interlaced: true}),
+        imagemin.jpegtran({progressive: true}),
+        imagemin.optipng({optimizationLevel: 5}),
+        imagemin.svgo({
+            plugins: [
+                {removeViewBox: true},
+                {cleanupIDs: false}
+            ]
+        })
+    ]))
+    .pipe( gulp.dest('assets/images'));
 });
